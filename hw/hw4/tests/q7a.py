ee45321e128812a97b0a2811dee27cee0595f30e
zhunation <danielzhu98@gmail.com>
2019-10-04 13:20:20 -0700

    push hw4

---
+++ hw/hw4/tests/q7a.py
@@ -0,0 +1,94 @@
+test = {   'name': 'q7a',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': ">>> bool(re.search(rt_re, 'hi "
+                                               "rt hello'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "'rt'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> bool(re.search(rt_re, 'rt "
+                                               "hello'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "'hello rt'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "'rthello'))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "'hellort'))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "':rt'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> bool(re.search(rt_re, '
+                                               "'rt:'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'#hello'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'hello#'))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'#'))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'#!'))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'#HELLO'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'http'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "''))\n"
+                                               'False',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> '
+                                               'bool(re.search(hash_link_re, '
+                                               "'httphttp'))\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
